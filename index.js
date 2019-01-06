0dafa41595cbd5e06824572c0cc572c927a52060
pinturavi <pintu541796@gmail.com>
2019-01-06 18:09:51 +0530

    changes before connecting mongo db

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const mongoose = requrie('mongoose')
 const logger = require('./middleware/logger')
 const authenticator = require('./middleware/authenticator')
 const genres = require('./routes/genres')
@@ -10,20 +11,24 @@ const morgan = require('morgan')
 const config = require('config')
 const app = express()
 
+mongoose.connect('mongodb://localhose/vidly', { useNewUrlParser: true })
+    .then(() => console.log('connected to mongo db.'))
+    .catch(error => console.log(error.message))
+
 app.set('view engine', 'pug')
 //this is default setting
 app.set('views', './views')
 
 app.use(express.json())
-app.use(express.urlencoded({extended : true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 app.use(helmet())
 app.use(logger)
 app.use(authenticator)
 app.use('/api/genres', genres)
-app.use('/', home)
+app.use('/', welcome)
 
-if('development' === app.get('env')) {
+if ('development' === app.get('env')) {
     app.use(morgan('tiny'))
     debug('morgan enabled in development mode')
 }
--- routes/genres.js
+++ routes/genres.js
@@ -1,47 +1,59 @@
 const express = require('express')
 const Joi = require('joi')
 const router = express.Router()
+const mongoose = require('mongoose')
+
+const Genre = mongoose.model('Genre', new mongoose.Schema({
+    name: {
+        type: Sting,
+        required: true,
+        min: 5,
+        max: 50
+    }
+}))
 
 const genres = [
-    {id:1, name:'action'},
-    {id:2, name:'romantic'},
-    {id:3, name:'thriller'}, 
-    {id:4, name:'fictional'}
+    { id: 1, name: 'action' },
+    { id: 2, name: 'romantic' },
+    { id: 3, name: 'thriller' },
+    { id: 4, name: 'fictional' }
 ]
 
 router.get('/', (req, res) => {
+    const genres = await Genre.find().sort('name')
+    if (!genre) return res.status(404).send(`No genres found`)
     res.send(genres)
 })
 
-router.get('/:id', (req, res) => {
-    const genre = genres.find(g => g.id === parseInt(req.params.id))
-    if(!genre) return res.status(404).send(`No genre found with the id ${req.params.id}`)
+router.get('/:id', async (req, res) => {
+    const genre = await Genre.find().sort('name')
+    if (!genre) return res.status(404).send(`No genre found with the id ${req.params.id}`)
     res.send(genre)
 })
 
 router.post('/', (req, res) => {
-    const {error} = validateGenre(req.body)
-    if(error) return res.status(400).send(error.details[0].message)
+    const { error } = validateGenre(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
     const genre = {
-        id : genres.length+1,
-        name : req.body.name
+        id: genres.length + 1,
+        name: req.body.name
     }
-    genres.push(genre)
+    Genre.save()
     res.send(genre)
 })
 
 router.put('/:id', (req, res) => {
     const genre = genres.find(g => g.id === parseInt(req.params.id))
-    if(!genre) return res.status(404).send(`no genres available with the id ${req.params.id}`)
-    const {error} = validateGenre(req.body)
-    if(error) return res.status(400).send(error.details[0].message)
+    if (!genre) return res.status(404).send(`no genres available with the id ${req.params.id}`)
+    const { error } = validateGenre(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
     genre.name = req.body.name
     res.send(genre)
 })
 
 router.delete('/:id', (req, res) => {
     const genre = genres.find(g => g.id === parseInt(req.params.id))
-    if(!genre) return res.status(404).send(`no genres found with the id ${req.params.id}`)
+    if (!genre) return res.status(404).send(`no genres found with the id ${req.params.id}`)
     const index = genres.indexOf(genre)
     genres.splice(index, 1)
     res.send(genre)
@@ -49,9 +61,9 @@ router.delete('/:id', (req, res) => {
 
 const validateGenre = (genre) => {
     const schema = {
-        name:Joi.string().min(3).required()
+        name: Joi.string().min(3).required()
     }
     return Joi.validate(genre, schema)
 }
 
-module.exports=router
+module.exports = router
